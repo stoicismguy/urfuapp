c398e5e2293f15d8d644a0c72152681a85078822
stoicism guy <pyzan55@gmail.com>
2023-12-08 16:59:00 +0500

    removable

--- static/base.js
+++ static/base.js
@@ -18,11 +18,32 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location = url + this.getAttribute('id') + '/preview';
     });
 
-    $('#del-button').on('click', function() {
-        parent = this.parentElement.parentElement.parentElement.parentElement;
+    $('.del-button').on('click', function() {
+        window.myDialog.showModal();
+        pk = this.parentElement.parentElement.parentElement.parentElement.parentElement.getAttribute('id');
+        console.log(pk);
+        parent = this.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("quiz-name")[0];
         console.log(parent);
+        window.myDialog.getElementsByClassName("dialog-title")[0].textContent = parent.textContent;
+        $(".confirm").unbind("click");
+        $(".confirm").on('click', function() {
+            delete_data(pk);
+        });
     });
 
-
+    const delete_data = (pk) => {
+        data_send = {}
+        const scrf = document.getElementsByName('csrfmiddlewaretoken');
+        console.log(scrf);
+        data_send['csrfmiddlewaretoken'] = scrf[0].value;
+        $.ajax({
+            type: 'POST',
+            data: data_send,
+            url: window.location.href + pk + '/' + 'delete',
+            success: function (response) {
+                    location.reload();
+                }
+            });
+    }
 
 });
